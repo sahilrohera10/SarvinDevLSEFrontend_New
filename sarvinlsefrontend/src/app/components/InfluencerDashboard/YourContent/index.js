652175d569cd1dd78f246aaa1fa199d8eb7a2ea0
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-09-11 10:09:46 +0530

    shown contents on your content and user-profile

--- sarvinlsefrontend/src/app/components/InfluencerDashboard/YourContent/index.js
+++ sarvinlsefrontend/src/app/components/InfluencerDashboard/YourContent/index.js
@@ -10,6 +10,7 @@ import {
 } from "react-social-media-embed";
 import Podium from "../../commons/icons/Business.png";
 import Image from "next/image";
+import ContentVideos from "../ContentVideos/index"
 
 const YourContent = ({ isTabletOrMobile = false }) => {
   return (
@@ -45,52 +46,13 @@ const YourContent = ({ isTabletOrMobile = false }) => {
       <div
         class={
           isTabletOrMobile
-            ? "flex flex-col justify-center align-middle mx-8 mt-4 gap-4 w-full "
-            : "flex mx-12 mt-4 gap-4 w-full flex-wrap"
+            ? "flex flex-col justify-center align-middle mx-8 mt-4 gap-4 w-fit "
+            : "flex px-12 mt-4 justify-around flex-wrap w-fit"
         }
       >
-        <InstagramEmbed
-          url="https://www.instagram.com/p/CUbHfhpswxt/"
-          width={328}
-          height={500}
-        />
-        <YouTubeEmbed
-          url="https://www.youtube.com/watch?v=HpVOs5imUN0"
-          width={328}
-          height={500}
-        />
 
-        <LinkedInEmbed
-          url="https://www.linkedin.com/embed/feed/update/urn:li:share:6898694772484112384"
-          postUrl="https://www.linkedin.com/posts/peterdiamandis_5-discoveries-the-james-webb-telescope-will-activity-6898694773406875648-z-D7"
-          width={328}
-          height={500}
-        />
-        <XEmbed
-          url="https://twitter.com/PixelAndBracket/status/1356633038717923333"
-          width={328}
-          height={500}
-        />
-        <YouTubeEmbed
-          url="https://youtu.be/zCsR-mlRR8M?si=DRHILYQQ4-yyU4Ms"
-          width={328}
-          height={500}
-        />
-        <FacebookEmbed
-          url="https://www.facebook.com/andrewismusic/posts/451971596293956"
-          width={328}
-          height={500}
-        />
-        <InstagramEmbed
-          url="https://www.instagram.com/p/CUbHfhpswxt/"
-          width={328}
-          height={500}
-        />
-        <YouTubeEmbed
-          url="https://www.youtube.com/watch?v=HpVOs5imUN0"
-          width={328}
-          height={500}
-        />
+        <ContentVideos />
+        
       </div>
     </div>
   );
