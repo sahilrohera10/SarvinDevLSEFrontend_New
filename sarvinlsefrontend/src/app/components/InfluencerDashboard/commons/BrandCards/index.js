3ccb3adbc4c984ed03dceecfd01373d3eacef088
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-10-27 20:23:36 +0530

    follow unfollow

--- sarvinlsefrontend/src/app/components/InfluencerDashboard/commons/BrandCards/index.js
+++ sarvinlsefrontend/src/app/components/InfluencerDashboard/commons/BrandCards/index.js
@@ -31,14 +31,16 @@ export default function BrandCard({
   type = 1,
   isSaved = false,
   is_followed = false,
+  onUnfollow = () => {},
 }) {
   const [openModal, setOpenModal] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const [isSave, setIsSave] = useState(isSaved);
   const [isFollowed, setIsFollowed] = useState(is_followed);
+  const [isLoading, setIsLoading] = useState(false);
   const SaveDealFunction = () => {
+    setIsLoading(true);
     const token = localStorage.getItem("token");
-    console.log(token);
     axios
       .post(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/save_deal`,
@@ -62,15 +64,19 @@ export default function BrandCard({
         } else {
           console.error("Axios request error:", error.message);
         }
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
   const FollowBrand = () => {
+    setIsLoading(true);
     const token = localStorage.getItem("token");
     axios
       .post(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/follow_brand`,
-        { brand_id: brandId }, 
+        { brand_id: brandId },
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -89,21 +95,24 @@ export default function BrandCard({
         } else {
           console.error("Axios request error:", error.message);
         }
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
   const UnfollowBrand = () => {
+    setIsLoading(true);
     const token = localStorage.getItem("token");
     axios
-      .delete(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/unfollow_brand`,
-        {
-          data: { brand_id: brandId },
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
+      .delete(`${process.env.NEXT_PUBLIC_BASE_URL}/api/user/unfollow_brand`, {
+        data: { brand_id: brandId },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
       .then(() => {
         setIsFollowed(false);
+        onUnfollow();
       })
       .catch((error) => {
         if (error.response) {
@@ -114,9 +123,11 @@ export default function BrandCard({
         } else {
           console.error("Axios request error:", error.message);
         }
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
-  
 
   // useEffect(() => {
   //   const interval = setInterval(() => {
@@ -193,7 +204,28 @@ export default function BrandCard({
                 onClick={SaveDealFunction}
                 disabled={isSave}
               >
-                {isSave ? "Deal Saved" : "Save Deal"}
+                {isSave ? (
+                  "Deal Saved"
+                ) : isLoading ? (
+                  <svg
+                    aria-hidden="true"
+                    class="w-6 h-6 mx-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#F27430]"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                ) : (
+                  "Save Deal"
+                )}
               </button>
             </div>
           )}
@@ -280,9 +312,49 @@ export default function BrandCard({
           <div className="text-gray-400 text-md mt-2 mr-2 w-full">
             <button
               class="flex w-full justify-center rounded-md bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => isFollowed?UnfollowBrand():FollowBrand()}
+              onClick={() => (isFollowed ? UnfollowBrand() : FollowBrand())}
             >
-              {isFollowed ? "UnFollow" : "Follow"}
+              {isFollowed ? (
+                isLoading ? (
+                  <svg
+                    aria-hidden="true"
+                    class="w-6 h-6 mx-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#F27430]"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                ) : (
+                  "Unfollow"
+                )
+              ) : isLoading ? (
+                <svg
+                  aria-hidden="true"
+                  class="w-6 h-6 mx-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#F27430]"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              ) : (
+                "Follow"
+              )}
             </button>
           </div>
         </div>
