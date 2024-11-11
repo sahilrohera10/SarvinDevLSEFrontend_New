4890b628172d079da252f9a2c061de3cd6c41e7e
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2024-11-12 00:44:37 +0530

    ony show contents if isBidActive=true

--- sarvinlsefrontend/src/app/components/BrandDashboard/commons/BidCards/index.js
+++ sarvinlsefrontend/src/app/components/BrandDashboard/commons/BidCards/index.js
@@ -1,15 +1,33 @@
 "use client";
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Image from "next/image";
 import BrandCard from "../BrandCards";
 import Podium from "../../../commons/icons/podium.png";
 import ViewSwitcher from "../../../commons/GridListToggle";
 import BrandListCard from "../BrandListCards";
-
 const BrandsDealCards = ({ text = null, children }) => {
   const [isListView, setIsListView] = useState(false);
-
+  const [brandDeals, setBrandDeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/brand/deal`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        const filteredDeals = response.data?.data.filter(deal => deal.isBidAvailable === true);
+        setBrandDeals(filteredDeals);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div style={{ margin: "0px 20px" }}>
       <div
@@ -31,11 +49,9 @@ const BrandsDealCards = ({ text = null, children }) => {
           <ViewSwitcher isListView={isListView} setIsListView={setIsListView} />
         </div>
       </div>
-
       <div style={{ fontSize: 14, fontWeight: 200, margin: "0px 20px" }}>
-        Lets Start Earning By Cracking The Best Suitable Deals For You{" "}
+        Let's Start Earning By Cracking The Best Suitable Deals For You
       </div>
-
       {isListView ? (
         <div
           style={{
@@ -45,54 +61,15 @@ const BrandsDealCards = ({ text = null, children }) => {
             flexWrap: "wrap",
           }}
         >
-          <BrandListCard
-            heading="Webneel.com"
-            subheading="Food Court and Cuisines"
-            img="https://webneel.com/daily/sites/default/files/images/daily/07-2014/5-best-ads-agasalho-hamburguer.jpg"
-            cardType="Deals"
-          />
-          <BrandListCard
-            heading="Juicy Juice"
-            subheading="Herbal Juice with ayurveda"
-            img="https://i.pinimg.com/736x/82/e6/0d/82e60d46a8bfb3917b32ab7caa8cab4d.jpg"
-            cardType="Deals"
-          />
-          <BrandListCard
-            heading="Fortis Hospital"
-            subheading="WHO Drive in clinic"
-            img="https://image.adsoftheworld.com/qbb857lhl8438y7np5x4ws4nos3e"
-            cardType="Deals"
-          />
-          <BrandListCard
-            heading="Pepsico Pvt.Ltd"
-            subheading="ColdDrink and Beverages"
-            img="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/651bcacd14d630b76b96cc73_8ucqCdESTiu3lL3oK2V5.jpeg"
-            cardType="Deals"
-          />
-          <BrandListCard
-            heading="Webneel.com"
-            subheading="Food Court and Cuisines"
-            img="https://webneel.com/daily/sites/default/files/images/daily/07-2014/5-best-ads-agasalho-hamburguer.jpg"
-            cardType="Deals"
-          />
-          <BrandListCard
-            heading="Juicy Juice"
-            subheading="Herbal Juice with ayurveda"
-            img="https://i.pinimg.com/736x/82/e6/0d/82e60d46a8bfb3917b32ab7caa8cab4d.jpg"
-            cardType="Deals"
-          />
-          <BrandListCard
-            heading="Fortis Hospital"
-            subheading="WHO Drive in clinic"
-            img="https://image.adsoftheworld.com/qbb857lhl8438y7np5x4ws4nos3e"
-            cardType="Deals"
-          />
-          <BrandListCard
-            heading="Pepsico Pvt.Ltd"
-            subheading="ColdDrink and Beverages"
-            img="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/651bcacd14d630b76b96cc73_8ucqCdESTiu3lL3oK2V5.jpeg"
-            cardType="Deals"
-          />
+          {brandDeals.map((deal) => (
+            <BrandListCard
+              key={deal.deal_id}
+              heading={deal.brand_name}
+              subheading={deal.product_description}
+              img={deal.product_photo}
+              cardType="Bid"
+            />
+          ))}
         </div>
       ) : (
         <div
@@ -103,59 +80,19 @@ const BrandsDealCards = ({ text = null, children }) => {
             flexWrap: "wrap",
           }}
         >
-          <BrandCard
-            heading="Webneel.com"
-            subheading="Food Court and Cuisines"
-            img="https://i.pinimg.com/474x/39/88/85/398885df3f6e523828d80cf867a77039.jpg"
-            cardType="Bid"
-          />
-          <BrandCard
-            heading="Juicy Juice"
-            subheading="Herbal Juice with ayurveda"
-            img="https://image.adsoftheworld.com/03qu7s1cl1ie8d2t4uqxmtrlocld"
-            cardType="Bid"
-          />
-          <BrandCard
-            heading="Fortis Hospital"
-            subheading="WHO Drive in clinic"
-            img="https://image.adsoftheworld.com/65bfpiteroq1oo5ueeg9h3o1bw5y"
-            cardType="Bid"
-          />
-          <BrandCard
-            heading="Pepsico Pvt.Ltd"
-            subheading="ColdDrink and Beverages"
-            img="https://image.adsoftheworld.com/klaa04bpgu4h2sjj6mxy95uzbw9b"
-            cardType="Bid"
-          />
-          <BrandCard
-            heading="Webneel.com"
-            subheading="Food Court and Cuisines"
-            img="https://webneel.com/daily/sites/default/files/images/daily/07-2014/5-best-ads-agasalho-hamburguer.jpg"
-            cardType="Bid"
-          />
-          <BrandCard
-            heading="Juicy Juice"
-            subheading="Herbal Juice with ayurveda"
-            img="https://i.pinimg.com/736x/82/e6/0d/82e60d46a8bfb3917b32ab7caa8cab4d.jpg"
-            cardType="Bid"
-            tags={2}
-          />
-          <BrandCard
-            heading="Fortis Hospital"
-            subheading="WHO Drive in clinic"
-            img="https://image.adsoftheworld.com/qbb857lhl8438y7np5x4ws4nos3e"
-            cardType="Bid"
-          />
-          <BrandCard
-            heading="Pepsico Pvt.Ltd"
-            subheading="ColdDrink and Beverages"
-            img="https://assets-global.website-files.com/63a9fb94e473f36dbe99c1b1/651bcacd14d630b76b96cc73_8ucqCdESTiu3lL3oK2V5.jpeg"
-            cardType="Bid"
-          />
+          {brandDeals.map((deal) => (
+            <BrandCard
+              key={deal.deal_id}
+              heading={deal.brand_name}
+              subheading={deal.product_description}
+              img={deal.product_photo}
+              cardType="Bid"
+              fixed_price={deal.fixed_price}
+            />
+          ))}
         </div>
       )}
     </div>
   );
 };
-
 export default BrandsDealCards;
